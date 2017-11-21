90c216cb184637bafe001fe7fd0f59456ed87aac
Puzhen Qian <puzhenq@gmail.com>
2017-11-21 22:47:20 +0800

    finish front end development

--- wechat/pages/favourite/favourite.js
+++ wechat/pages/favourite/favourite.js
@@ -6,6 +6,7 @@ Page({
    */
   data: {
     favourites: [],
+    conflicts: [],
     weekdays:['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   },
 
@@ -78,16 +79,87 @@ Page({
     this.findConflicts();
   },
 
+  // This function finds all the time conflict of the user's favourite
+  // courses and create suitable data for rendering
+  // A conflict object looks like this:
+  //  {course1Id: '1', course2Id: '2', conflictTime: ['6', '7', '8']}
   findConflicts: function() {
     // STEP 1, find all the slots
-    var maxColumns = 0;
     var favourites = this.data.favourites;
-    for (var i = 0; i < favourites.length; i++) {
-      numberOfColumns = favourites[i].weekTime.length;
-      // TODO 
+    var conflicts = [];
+    for (var i = 0; i < favourites.length - 1; i++) {
+      var course1 = favourites[i];
+      for (var j = i + 1; j < favourites.length; j++) {
+        var course2 = favourites[j];
+        // check week time conflict first
+        var hasConflict = false;
+        for (var k = 0; k < course1.weekTime.length; k++) {
+          var aWeekTime = course1.weekTime[k];
+          // if aWeekTime of course1 is contained in course2's time table
+          if (this.arrayContainsElement(course2.weekTime, aWeekTime)) {
+            hasConflict = true;
+            break;
+          }
+        }
+        if (hasConflict) {
+          var course1ClassTime = this.computeClassTime(course1);
+          var course2ClassTime = this.computeClassTime(course2);
+          var conflictTime = []
+          for (var k = 0; k < course1ClassTime.length; k++) {
+            var currentTime = course1ClassTime[k];
+            if (this.arrayContainsElement(course2ClassTime, currentTime)) {
+              conflictTime.push(currentTime);
+            }
+          }
+          if (conflictTime.length > 0) {
+            conflicts.push({
+              course1: i, course2: j,
+              conflictTime: conflictTime
+            });
+          }
+        }
+      }
     }
     // STEP 2, find if there is any conflict in this slot, record it
     // STEP 3, render it using wxml
+    this.setData({
+      conflicts: conflicts
+    });
+    console.log('logging out conflicts: ');
+    console.log(conflicts);
+  },
+
+  // The input course object should conform to "favourites" standard
+  // This function creates an array representing the class time for 
+  // one course. The elements are like "1-6".
+  computeClassTime: function(course) {
+    var classTime = [];
+    if (course.weekTime.length === 1) {
+      for (var i = 0; i < course.classTime.length; i++) {
+        classTime.push(course.weekTime[0] + '-' + course.classTime[i]);
+      }
+    } else {
+      for (var i = 0; i < course.weekTime.length; i++) {
+        var weekNumber = course.weekTime[i];
+        for (var j = 0; j < course.classTime[i].length; j++) {
+          classTime.push(weekNumber + '-' + course.classTime[i][j]);
+        }
+      }
+    }
+    return classTime;
+  },
+
+  // This function can be widely applied, check if an element 
+  // is contained in an array.
+  arrayContainsElement: function(array, element) {
+    var contain = false;
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] === element) {
+        contain = true;
+        break;
+      }
+    }
+    return contain;
   },
 
   /**
