641cb0086beff7de32bbb2caf1cbc68fccc453c5
yeiamx <925166430@qq.com>
2017-11-22 10:47:46 +0800

    return the backend to eclipse form

--- backend/CourseApp/src/main/edu/zjgsu/CourseApp/servlet/CourseRateServlet.java
+++ backend/CourseApp/src/main/edu/zjgsu/CourseApp/servlet/CourseRateServlet.java
@@ -5,7 +5,6 @@ import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,7 +16,6 @@ import edu.zjgsu.CourseApp.service.impl.StudentAttendedCoursesServiceImpl;
 /**
  * Servlet implementation class CourseRateServlet
  */
-@WebServlet("/rate")
 public class CourseRateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	public StudentAttendedCoursesService sacService;   
